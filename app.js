4437ab8c456bd53a7890df0113883bb6ab341b9d
Konstantin Kuts <karzuan@gmail.com>
2020-04-11 13:07:11 +0800

    add new templates, created layout.pug template to keep all repeated code in templates

--- app.js
+++ app.js
@@ -2,13 +2,36 @@ const express = require('express');
 
 const app = express();
 
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple'
+  ]; 
+  const friends = [
+    ['Illya', 'Shingarev'],
+    ['Maksim', 'Fokeev'],
+    ['Many', 'Davao'],
+    ['Don', 'BlackNiger'],
+    ['Pepper', 'Who']
+  ]; 
+  const username ="Kosta";
+
 app.set('view engine', 'pug');
+//app.set('views', __dirname + '/templates') example of changing default views directory
 
 app.get('/', (req, res) => {
     res.render('index');
 } );
-app.get('/hello', (req, res) => {
-    res.send('<h1>Hello, Javascript Developer!</h1>');
+app.get('/cards', (req, res) => {
+    //res.locals.prompt = "Who1 us buries in Grant's tomb?";
+    res.render('card', {prompt: "Who1 us buries in Grant's tomb?", hint: "Think about whose tomb it is."});
+} );
+app.get('/sandbox', (req, res) => {
+    //res.locals.prompt = "Who1 us buries in Grant's tomb?";
+    res.render('sandbox', {username, friends});
 } );
 
 app.listen(3000, () => {
