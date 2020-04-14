23a097ba02df3f4dd8c6a5c87c2ee88a1a840a3b
Konstantin Kuts <karzuan@gmail.com>
2020-04-14 08:39:15 +0800

    add hello route, and template. It works with get and post. Installed body-parser extention. Now if I submit a name it will show in terminal body parsed request

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+// start the app: nodemon
 const express = require('express');
+const bodyParses = require('body-parser');
+
 
 const app = express();
+app.use(bodyParses.urlencoded({ extended: false}));
 
 const colors = [
     'red',
@@ -32,6 +36,13 @@ app.get('/cards', (req, res) => {
 app.get('/sandbox', (req, res) => {
     //res.locals.prompt = "Who1 us buries in Grant's tomb?";
     res.render('sandbox', {username, friends});
+} );
+app.get('/hello', (req, res) => {
+    res.render('hello');
+} );
+app.post('/hello', (req, res) => {
+    console.dir(req.body);
+    res.render('hello');
 } );
 
 app.listen(3000, () => {
