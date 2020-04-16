d4810ab597ffba23cc68cd9a86bfca953da14049
Konstantin Kuts <karzuan@gmail.com>
2020-04-16 21:48:20 +0800

    working with cookies, routes and redirects

--- app.js
+++ app.js
@@ -29,8 +29,19 @@ app.set('view engine', 'pug');
 //app.set('views', __dirname + '/templates') example of changing default views directory
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const name = req.cookies.username;
+    if (name){
+    res.render('index', {name: name });
+    } else {
+        res.redirect('hello');
+    }
 } );
+
+app.post('/goodbye', (req, res) => {
+    res.clearCookie('username');
+    res.redirect('/hello');
+} );
+
 app.get('/cards', (req, res) => {
     //res.locals.prompt = "Who is buried in Grant's tomb?";
     res.render('card', {prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is."});
@@ -40,13 +51,18 @@ app.get('/sandbox', (req, res) => {
     res.render('sandbox', {username, friends});
 } );
 app.get('/hello', (req, res) => {
-    res.render('hello', {name: req.cookies.username});
+    if ( req.cookies.username ) {
+        res.redirect('/');
+    } else {
+    res.render('hello');
+    }
 } );
 app.post('/hello', (req, res) => {
     res.cookie('username', req.body.username );
     //console.dir(req.body);
     //res.json(req.body);
-    res.render('hello', {name: req.body.username });
+    //res.render('hello', {name: req.body.username });
+    res.redirect('/');
 } );
 
 app.listen(3000, () => {
