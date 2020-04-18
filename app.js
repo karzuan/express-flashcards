e26c26c03d32d3c51e22568234aaba12ee8b02d9
Konstantin Kuts <karzuan@gmail.com>
2020-04-18 19:18:48 +0800

    Latest changess, app connected to design

--- app.js
+++ app.js
@@ -1,41 +1,33 @@
-// to start the app: node app.js || nodemon
 const express = require('express');
-const bodyParses = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const mainRoutes = require('./routes/index.js');
-const cardRoutes = require('./routes/cards.js');
-
 
 const app = express();
-app.use(bodyParses.urlencoded({ extended: false}));
+
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
+app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
+
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
+
 app.use(mainRoutes);
 app.use('/cards', cardRoutes);
 
-//app.set('views', __dirname + '/templates') example of changing default views directory
-
-// app.use((req, res, next ) => {
-//     console.log('Hello');
-//     const err = new Error('Oh noes!');
-//     err.status = 500;
-//     next(err);
-// });
-
-app.use((req, res, next )=>{
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.use((err, req, res, next)=>{
-    res.locals.error = err;
-    res.status(err.status);
-    res.render('error', err);
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
 });
 
 app.listen(3000, () => {
-    console.log('The application is runnung on a localgost:3000')
+    console.log('The application is running on localhost:3000!')
 });
-
--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-const colors = [
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'purple'
-  ]; 
-  const friends = [
-    ['Illya', 'Shingarev'],
-    ['Maksim', 'Fokeev'],
-    ['Many', 'Davao'],
-    ['Don', 'BlackNiger'],
-    ['Pepper', 'Who']
-  ]; 
-  const username ="Kosta";
-
-
 router.get('/', (req, res) => {
     const name = req.cookies.username;
-    if (name){
-    res.render('index', {name: name });
+    if (name) {
+      res.render('index', { name });
     } else {
-        res.redirect('hello');
+      res.redirect('/hello');
     }
-} );
+});
+
+router.get('/hello', (req, res) => {
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect('/');
+  } else {
+    res.render('hello');
+  }
+});
+
+router.post('/hello', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/');
+});
 
 router.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('/hello');
-} );
-
-router.get('/sandbox', (req, res) => {
-    res.render('sandbox', {username, friends});
-} );
-router.get('/hello', (req, res) => {
-    if ( req.cookies.username ) {
-        res.redirect('/');
-    } else {
-    res.render('hello');
-    }
-} );
-router.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username );
-    //console.dir(req.body);
-    //res.json(req.body);
-    //res.render('hello', {name: req.body.username });
-    res.redirect('/');
-} );
+  res.clearCookie('username');
+  res.redirect('/hello');
+});
 
 module.exports = router;
